a4caa3f55f5f64c99bdcc41b64dcb4769ac77a4a
Arayve <alejandro150998@outlook.com>
2018-05-19 19:47:00 -0300

    Modificando estructuras basicas

--- abb.c
+++ abb.c
@@ -1,17 +1,17 @@
 #include "abb.h"
 
+
+typedef struct abb_hijo {
+	struct abb_hijo* izq;
+	struct abb_hijo* der;
+}abb_hijo_t;
+
 struct abb {
-	
-	abb_t* izq;
-	abb_t* der;
+	abb_hijo_t* izq;
+	abb_hijo_t* der;
 	abb_comparar_clave_t // son punteros aca 
 	abb_destruir_dato_t
 };
-
-typedef struct abb_hijo {
-    void* valor;
-    struct nodo* prox;
-}abb_hijo_t;
 
 abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
 
