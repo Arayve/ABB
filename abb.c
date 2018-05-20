0c4ca1a9f8e703a50287f1dcc9b4c6e8c3ca2b25
KethTatsu <ben.inneo@gmail.com>
2018-05-20 18:15:41 -0300

    Agregadas funciones de iter, agregados pila.c y pila.h, agregada descripcion de funciones en abb.h

--- abb.c
+++ abb.c
@@ -1,9 +1,10 @@
 #include "abb.h"
+#include "pila.h"
 
 typedef struct campo{
 	char* clave;
 	void* dato;
-}campo_t;	//Se puede cambiar a nombre más descriptivo/nodo_t
+}campo_t;	//Se puede cambiar a nombre más descriptivo
 
 typedef struct abb_nodo{
 	campo_t* campo;
@@ -17,6 +18,11 @@ struct abb{	//VER DIAPOSITIVA DE ABB
 	abb_comparar_clave_t cmp;
 	abb_destruir_dato_t destruir_dato;
 };
+
+struct abb_iter{ //VERIFICAR
+	pila_t* pila_abb;
+};
+
 
 /*
 typedef struct abb_hijo {
@@ -63,26 +69,60 @@ void abb_destruir(abb_t *arbol){
 
 //ITERADOR INTERNO
 
-void abb_in_order(abb_t *arbol, bool visitar(const char *, void *, void *), void *extra);
+void abb_in_order(abb_t *arbol, bool visitar(const char *, void *, void *), void *extra){
+
+
+}
 
 //ITERADOR EXTERNO
 
 abb_iter_t *abb_iter_in_crear(const abb_t *arbol){
 
+	abb_iter_t* iter_nuevo = malloc(sizeof(abb_iter_t));
+
+	if(!iter_nuevo) return NULL;
+
+	iter_nuevo->pila_abb = pila_crear();
+
+	if(!iter_nuevo->pila_abb){
+		free(iter_nuevo);
+		return NULL;
+	}
+
+	pila_apilar(iter_nuevo->pila_abb, arbol->raiz);
+
+	//APILAR TODOS LOS HIJOS IZQ
+
+	return iter_nuevo;
 }
 
 bool abb_iter_in_avanzar(abb_iter_t *iter){
 
+	if(abb_iter_in_al_final(iter)) return false;
+
+	abb_nodo_t* nodo = pila_desapilar(iter->pila_abb);
+
+	//APILO HIJOS DERECHOS DEL DESAPILADO
+
+	return true;
 }
 
 const char *abb_iter_in_ver_actual(const abb_iter_t *iter){
 
+	if(abb_iter_in_al_final(iter)) return NULL;
+
+	abb_nodo_t* nodo = pila_ver_tope(iter->pila_abb);
+
+	return nodo->campo->clave;
 }
 
 bool abb_iter_in_al_final(const abb_iter_t *iter){
 
+	return (pila_esta_vacia(iter->pila_abb));
 }
 
 void abb_iter_in_destruir(abb_iter_t* iter){
 
+	pila_destruir(iter->pila_abb);
+	free(iter);
 }
