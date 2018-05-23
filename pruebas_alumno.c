b65af7e09b6671c1f5a052e5d65fede98da36382
Arayve <alejandro150998@outlook.com>
2018-05-23 02:32:02 -0300

    Error en la primitivas  guardar

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -47,6 +47,30 @@ void prueba_abb_guardar_varios(){
 
 	printf("PRUEBA ABB GUARDAR VARIOS\n");
 
+	abb_t* abb = abb_crear(strcmp, NULL);
+	char* clave1 = "Arg", *dato1 = "Messi";
+	char* clave2 = "Bra", *dato2 = "Hulk";
+	char* clave3 = "Bra", *dato3 = "Pato";
+	char* clave4 = "Chi", *dato4 = "Vidal";
+	char* clave5 = "Por", *dato5 = "Ronaldo";
+	char* clave6 = "Esp", *dato6 = "Puyol";
+	char* clave7 = "Col", *dato7 = "Falcao";
+	char* clave8 = "Uru", *dato8 = "Suarez";
+
+
+	print_test("Se creó abb", abb != NULL);
+	print_test("abb_cantidad igual a 0", abb_cantidad(abb) == 0);
+	print_test("Se guardó clave", abb_guardar(abb, clave1, dato1));
+	print_test("Se guardó clave", abb_guardar(abb, clave2, dato2));
+	print_test("Se guardó clave", abb_guardar(abb, clave3, dato3));
+	print_test("Se guardó clave", abb_guardar(abb, clave4, dato4));
+	print_test("Se guardó clave", abb_guardar(abb, clave5, dato5));
+	print_test("Se guardó clave", abb_guardar(abb, clave6, dato6));
+	print_test("Se guardó clave", abb_guardar(abb, clave7, dato7));
+	print_test("Se guardó clave", abb_guardar(abb, clave8, dato8));
+	print_test("abb_cantidad igual a 7", abb_cantidad(abb) == 7);
+
+
 
 }
 
