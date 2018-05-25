45c01edbf9697c16bd86deae39ad5594e115ee3a
KethTatsu <ben.inneo@gmail.com>
2018-05-25 19:16:01 -0300

    Agregadas pruebas del iter

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 #include <stddef.h> 
 
+#define TAM 5000
 
 void prueba_abb_vacio(){
 
-	printf("PRUEBA ABB VACIO\n");
+	printf("\nPRUEBA ABB VACIO\n");
 
 	abb_t* abb_vacio = abb_crear(strcmp, NULL);
 	char* clave = "random";
@@ -25,7 +26,7 @@ void prueba_abb_vacio(){
 
 void prueba_abb_guardar_y_borrar(){
 
-	printf("PRUEBA ABB GUARDAR Y BORRAR\n");
+	printf("\nPRUEBA ABB GUARDAR Y BORRAR\n");
 
 	abb_t* abb = abb_crear(strcmp, NULL);
 	char* clave = "Arg", *dato = "Messi";
@@ -43,7 +44,8 @@ void prueba_abb_guardar_y_borrar(){
 	abb_destruir(abb);
 }
 void prueba_abb_guardar_2_elementos(){
-	printf("PRUEBA ABB GUARDAR 2 ELEMENTOS\n");
+
+	printf("\nPRUEBA ABB GUARDAR 2 ELEMENTOS\n");
 
 	abb_t* abb = abb_crear(strcmp, NULL);
 	char* clave1 = "Arg", *dato1 = "Messi";
@@ -62,7 +64,7 @@ void prueba_abb_guardar_2_elementos(){
 }
 void prueba_abb_guardar_varios(){
 
-	printf("PRUEBA ABB GUARDAR VARIOS\n");
+	printf("\nPRUEBA ABB GUARDAR VARIOS\n");
 
 	abb_t* abb = abb_crear(strcmp, NULL);
 
@@ -101,7 +103,7 @@ void prueba_abb_guardar_varios(){
 	print_test("clave8 obtener dato9", abb_obtener(abb, clave8) == dato9);
 
 	abb_destruir(abb);
-}/*
+}
 void prueba_abb_volumen(){
 
 	printf("PRUEBA ABB VOLUMEN\n");
@@ -112,21 +114,133 @@ void prueba_abb_volumen(){
 	unsigned long datos[TAM];
 
 	for(unsigned long i = 0; i < TAM; i++){
-		sprintf(claves[i], "%08d", i);
+		sprintf(claves[i], "%08lu", i);
 		datos[i] = i;
-		if(!abb_guardar(abb, claves[i], datos[i])){
+		if(!abb_guardar(abb, claves[i], &datos[i])){
 			print_test("NO se pudieron guardar todas las claves/datos", false);
 		}
 	}
 
+	print_test("Se guardaron 5000 elementos", true);
+
+	abb_destruir(abb);
 }
-*/
+
+void prueba_abb_iter(){
+
+	abb_t* abb = abb_crear(strcmp, NULL);
+
+	printf("\nPRUEBA ABB ITER\n");
+
+	char* clave1 = "Bra", *dato1 = "Hulk";
+	char* clave2 = "Esp", *dato2 = "Puyol";
+	char* clave3 = "Ing", *dato3 = "Kane";
+	char* clave4 = "Chi", *dato4 = "Vidal";
+	char* clave5 = "Uru", *dato5 = "Suarez";
+	char* clave6 = "Por", *dato6 = "Ronaldo";
+	char* clave7 = "Col", *dato7 = "Falcao";
+	char* clave8 = "Arg", *dato8 = "Messi";
+
+	print_test("Se creó abb", abb != NULL);
+
+	print_test("Se guardó clave1", abb_guardar(abb, clave1, dato1));
+	print_test("Se guardó clave2", abb_guardar(abb, clave2, dato2));
+	print_test("Se guardó clave3", abb_guardar(abb, clave3, dato3));
+	print_test("Se guardó clave4", abb_guardar(abb, clave4, dato4));
+	print_test("Se guardó clave5", abb_guardar(abb, clave5, dato5));
+	print_test("Se guardó clave6", abb_guardar(abb, clave6, dato6));
+	print_test("Se guardó clave7", abb_guardar(abb, clave7, dato7));
+	print_test("Se guardó clave8", abb_guardar(abb, clave8, dato8));
+
+	abb_iter_t* iter = abb_iter_in_crear(abb);
+
+	print_test("Se creó iter", iter != NULL);
+
+	print_test("Ver actual es clave8", strcmp(abb_iter_in_ver_actual(iter), clave8) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave8);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave1", strcmp(abb_iter_in_ver_actual(iter), clave1) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave1);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave4", strcmp(abb_iter_in_ver_actual(iter), clave4) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave4);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave7", strcmp(abb_iter_in_ver_actual(iter), clave7) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave7);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave2", strcmp(abb_iter_in_ver_actual(iter), clave2) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave2);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave3", strcmp(abb_iter_in_ver_actual(iter), clave3) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave3);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave6", strcmp(abb_iter_in_ver_actual(iter), clave6) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave6);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Ver actual es clave5", strcmp(abb_iter_in_ver_actual(iter), clave5) == 0);
+	print_test("Ver actual clave no es el mismo puntero", abb_iter_in_ver_actual(iter) != clave5);
+	print_test("Avanzar es true", abb_iter_in_avanzar(iter));
+	print_test("Iter está al final", abb_iter_in_al_final(iter));
+
+	abb_iter_in_destruir(iter);
+	abb_destruir(abb);
+}
+
+bool imprimir_claves_datos(const char* clave, void* dato, void* extra){
+
+	size_t* contador = extra;
+
+	printf("%lu. Clave: %s\tDato: %s\n",(*contador), clave, (char*)dato);
+
+	(*contador)++;
+
+	return true;
+}
+
+void prueba_abb_iter_interno(){
+
+	printf("\nPRUEBA ABB ITER INTERNO\n");
+
+	abb_t* abb = abb_crear(strcmp, NULL);
+
+	print_test("Se creó abb", abb != NULL);
+
+	char* clave1 = "Bra", *dato1 = "Hulk";
+	char* clave2 = "Esp", *dato2 = "Puyol";
+	char* clave3 = "Ing", *dato3 = "Kane";
+	char* clave4 = "Chi", *dato4 = "Vidal";
+	char* clave5 = "Uru", *dato5 = "Suarez";
+	char* clave6 = "Por", *dato6 = "Ronaldo";
+	char* clave7 = "Col", *dato7 = "Falcao";
+	char* clave8 = "Arg", *dato8 = "Messi";
+
+	print_test("Se guardó clave1", abb_guardar(abb, clave1, dato1));
+	print_test("Se guardó clave2", abb_guardar(abb, clave2, dato2));
+	print_test("Se guardó clave3", abb_guardar(abb, clave3, dato3));
+	print_test("Se guardó clave4", abb_guardar(abb, clave4, dato4));
+	print_test("Se guardó clave5", abb_guardar(abb, clave5, dato5));
+	print_test("Se guardó clave6", abb_guardar(abb, clave6, dato6));
+	print_test("Se guardó clave7", abb_guardar(abb, clave7, dato7));
+	print_test("Se guardó clave8", abb_guardar(abb, clave8, dato8));
+	
+	printf("\nImprimir claves y datos:\n");
+
+	size_t contador = 1;
+
+	abb_in_order(abb, imprimir_claves_datos, &contador);
+
+	//AGREGAR OTROS CASOS
+
+	abb_destruir(abb);
+}
+
 void pruebas_abb_alumno(){
 	
 	prueba_abb_vacio();
 	prueba_abb_guardar_y_borrar();
 	prueba_abb_guardar_2_elementos();
-	prueba_abb_guardar_varios();
-	//prueba_abb_volumen();
-
+//	prueba_abb_guardar_varios();
+//	prueba_abb_volumen();
+//	prueba_abb_iter();
+	prueba_abb_iter_interno();
 }
